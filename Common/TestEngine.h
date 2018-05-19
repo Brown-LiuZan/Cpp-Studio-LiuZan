6c587832e8419ec8c1f8b17da0c5b3426e278992
Brown-LiuZan <brown.liuzan@outlook.com>
2018-05-19 23:48:46 +0800

    Add TestCase and TestEngine classes into TestEngine.h

--- Common/TestEngine.h
+++ Common/TestEngine.h
@@ -2,6 +2,35 @@
 #define TEST_ENGINE_H
 
 
+#include <string>
+#include <chrono>
+
+
+class TestCase
+{
+private:
+    std::string mName;
+    std::chrono::seconds mTimeout;
+public:
+    TestCase(std::string & const inName, std::chrono::seconds inTimeout)
+        : mName {inName}, mTimeout {inTimeout}
+    {}
+
+    TestCase(std::string && inName, std::chrono::seconds inTimeout)
+        : mName {inName}, mTimeout {inTimeout}
+    {}
+
+    std::string & const GetName() const { return mName; }
+
+    std::chrono::seconds GetTimeout() const { return mTimeout; }
+
+    virtual bool Run();
+};
+
+
+class TestEngine
+{
+};
 
 
 #endif //TEST_ENGINE_H
