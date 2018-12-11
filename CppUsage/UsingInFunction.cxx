6e8b15651ef0467d980b77137d4529a24d78e605
Brown-LiuZan <brown.liuzan@outlook.com>
2018-12-11 21:17:42 +0800

    Add "UsingInFunction.cxx"

---
+++ CppUsage/UsingInFunction.cxx
@@ -0,0 +1,39 @@
+#include <assert.h>
+#include <iostream>
+#include <vector>
+
+//#define GTEST_DISABLED
+
+static void RunTest()
+{
+    using namespace std;
+
+    cout << "Begin of usage test of 'void func() {using namespace std; ...}'";
+    cout << endl;
+
+    vector<int> vVec = {0, 1, 2, 3};
+    for (int i = 0; i < vVec.size(); ++i)
+    {
+#ifdef GTEST_DISABLED
+        assert(vVec[i] == i);
+#else
+        EXPECT_EQ(vVec[i], i);
+#endif
+    }
+
+    cout << "You could use 'using namespace std;' in function." << endl;
+    cout << "End of usage test of 'void func() {using namespace std; ...}'";
+    cout << endl;
+}
+
+#ifdef GTEST_DISABLED
+int main(void)
+#else
+TEST(CppUsage, UsingInFunction)
+#endif
+{
+    RunTest();
+#ifdef GTEST_DISABLED
+    return 0;
+#endif
+}
